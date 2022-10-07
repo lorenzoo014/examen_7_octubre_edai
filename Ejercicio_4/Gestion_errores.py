80fc6d33927409c98fe7e5e8f3991afed836976c
lorenzoo22 <lagudmarr@gmail.com>
2022-10-07 16:35:16 +0200

    Ejercicio_4-terminado

--- Ejercicio_4/Gestion_errores.py
+++ Ejercicio_4/Gestion_errores.py
@@ -1,25 +1,34 @@
 class Errores():
+    @staticmethod
     def division_cero():
         try:
             numero = 7/0
         except ZeroDivisionError:
             print("No se puede dividir entre 0")
+    @staticmethod
     def lista_indice():
         try:
             lista = [4, 7, 30, 23, 5]
             lista[10]    
         except IndexError:
             print("Se esta intentando sacar un elemento superior o inferior al tamaño de la lista")
-
+    @staticmethod
     def diccionario_error():
         try:
             paises = { "españa":"español", "eeuu":"inglés", "italia":"italiano" } 
             paises["alemania"]
         except KeyError:
             print("el país introducido no se encuentra en la lista")
-    def lista_indice():
+    @staticmethod
+    def suma_error():
         try:
             resultado = "2" + 10
         except TypeError:
             resultado= int("2") + 10
             print("can only concatenate str (not int) to str")
+
+Errores.division_cero()
+Errores.lista_indice()
+Errores.diccionario_error()
+Errores.suma_error()
+
