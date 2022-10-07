7275f2980044bbc818d19e8d750acc5b21461151
lorenzoo22 <lagudmarr@gmail.com>
2022-10-07 17:25:09 +0200

    Ejercicio_5

--- Ejercicio_5/main.py
+++ Ejercicio_5/main.py
@@ -1,12 +1,21 @@
 #lanzador
 import sys
 sys.path.insert(0,"/Users/Lorenzo/Documents/programacion/1.EDAI_2/examen_7_octubre_edai")
-from Vehiculo import Vehiculo
+from Vehiculo import Vehiculo,Camioneta,Bicicleta,Motocicleta,Coche,catalogar,catalogar
 
 
 
 
+camioneta1 = Camioneta("rojo",4,130,1100,500)
+bicicleta1 = Bicicleta("verde",2,"urbana")
+Motocicleta1 = Motocicleta("naranja",2,"deportiva",120,1000)
+c = Coche("azul", 4, 150, 1200)
 
+lista_vehiculos =[]
+lista_vehiculos.append(c)
+lista_vehiculos.append(camioneta1)
+lista_vehiculos.append(bicicleta1)
+lista_vehiculos.append(Motocicleta1)
 
 
 def pruebas():
@@ -14,50 +23,20 @@ def pruebas():
         print("========================")
         print(" BIENVENIDO AL Manager ")
         print("========================")
-        print("[1] Ejercicio_1 ")
-        print("[2] Ejercicio_2 ")
-        print("[3]  Ejercicio_3 ")
-        print("[4]  Ejercicio_4 ")
-        print("[5] Cerrar el Manager ")
+        print("[1]  Funciones_catalogar ")
+        print("[2] Cerrar el Manager ")
         print("========================")
         opcion = input("> ")
         #--------1era_opcion--------#
-        if opcion == '1':
-            print("Ejercicio_1 con la siguiente matriz...\n")
-            print("[1,1,1,3]")
-            print("[2,2,2,7]")
-            print("[3,3,3,9]")
-            print("[4,4,4,13]")
-            print(Ejercicio1.sumaMatrices(matriz))
-        #--------2da_opcion--------#
-        if opcion == "2":
-            print("Ejercicio_2 con los siguientes strings...\n")
-            print("Hola")
-            print("nochevieja")
-            print("fun__fun__fun")
-            print("adios")
-            Ejercicio2.cadena("Hola")
-            Ejercicio2.cadena("nocheviejaa")
-            Ejercicio2.cadena("fun__fun__fun")
-            Ejercicio2.cadena("adios")
+        if opcion == "1":
+            print("\nComprobamos la funcion catalogar: ")
+            catalogar(lista_vehiculos)
+            print("\n Comprobamos la funcion catalogar con el polimorfismo de sobreecarga correspondiente : ")
+            catalogar(lista_vehiculos, 2)
+            catalogar(lista_vehiculos, 4)
+            catalogar(lista_vehiculos, 0)
 
-        if opcion == "3":
-            print("Ejercicio_3 ...\n")
-            print("del 0 al 10")
-            Ejercicio3.añadeElementos2(0,10)
-            print("del -10 al 0")
-            Ejercicio3.añadeElementos2(-10,0)
-            print("del 0 al 20 pares")
-            Ejercicio3.añadeElementos2(0,10,2)
-            print("del -20 al 0")
-            Ejercicio3.añadeElementos2(-19,0,2)
-            print("del 0 al 50")
-            Ejercicio3.añadeElementos2(0,50,5)
-        if opcion == "4":
-            print("Ejercicio_4 ...\n")
-            tabla.crearTabla(5,5)
-
-        if opcion == '5':
+        if opcion == '2':
             print("Saliendo...\n")
             break
 input("\nPresiona ENTER para continuar...")
